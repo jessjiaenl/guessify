3c8a64e55856b570e48638c0976ee12ee79fb3d2
JIAENL <128503379+jessjiaenl@users.noreply.github.com>
2025-04-14 22:21:40 -0400

    feat(schema): added db schemas

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "drizzle-kit"
 
 export default defineConfig({
     out: "./migrations",
-    schema: "./database/schema.ts",
+    schema: "./database/schema/*",
     dialect: "postgresql",
     dbCredentials: {
         url: process.env.DATABASE_URL!
