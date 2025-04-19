fac0bd0355b6611db1ba7c5e71fbb27078ab122a
JIAENL <128503379+jessjiaenl@users.noreply.github.com>
2025-04-19 09:57:10 -0400

    feat(profile): first implementation of profile page framework

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,9 +1,16 @@
+import { auth } from "@/lib/auth"
+import { headers } from "next/headers"; 
+
+import { ProfileView } from "./view"
+
 export default async function ProfilePage() {
-    return (
-        <main className="py-8 px-4">
-            <section className="container mx-auto">
-                <h1 className="text-2xl font-bold mb-6">My Profile</h1>
-            </section>
-        </main>
-    )
+    const session = await auth.api.getSession({
+        headers: await headers(),
+    });
+
+    if (!session) {
+        return null;
+    }
+
+    return <ProfileView />
 }
