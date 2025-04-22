916f3380af89059f5717c874c4e1862c48989d55
JIAENL <128503379+jessjiaenl@users.noreply.github.com>
2025-04-22 15:48:21 -0400

    feat(middleware): implemented middleware redirection for profile, leaderboard, bookmarks, history

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,33 @@
 
 import { type NextRequest, NextResponse } from "next/server"
+import { headers } from "next/headers"
+import { auth } from "@/lib/auth"
+
+const loggedinRoutes = [
+    "/profile",
+    "/leaderboard",
+    "/bookmarks",
+    "/history"
+]
 
 export async function middleware(request: NextRequest) {
     /* Implement a redirecting middleware YOUR CODE HERE */
+    const path = request.nextUrl.pathname;
+    const isLoggedinRoute = loggedinRoutes.includes(path);
+
+    const session = await auth.api.getSession({
+        headers: await headers(),
+    });
+
+    // not logged in but trying to access a page that needs to log in
+    if (!session && isLoggedinRoute) {
+        return NextResponse.redirect(new URL("/auth/sign-in", request.url));
+    }
 
     return NextResponse.next()
 }
 
 export const config = {
     runtime: "nodejs",
-    matcher: [/* TODO: Add paths to match */]
+    matcher: ["/profile", "/leaderboard", "/bookmarks", "/history"]
 }
